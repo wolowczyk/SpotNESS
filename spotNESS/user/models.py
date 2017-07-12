3c2b55cefbc9a459c1b56b82bfc3fbb11aa0a0a9
Pawel Wolowczyk <pawelwolowczyk@gmail.com>
2017-07-12 10:38:21 +0200

    updating map view

--- spotNESS/user/models.py
+++ spotNESS/user/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from geopy.geocoders import Nominatim
 
 
 class SpotUser(AbstractUser):
     username = models.CharField(max_length=27, unique=True)
     email = models.EmailField(unique=True)
     localization = models.CharField(max_length=27)
+
+    @classmethod
+    def get_position(cls, localization):
+        geolocator = Nominatim()
+        position = geolocator.geocode(localization)
+        return '{lat: {lat}, lng: {lng}'.format(lat=position.latitude, lng=position.longitude)
--- spotNESS/user/static/user/spotness.js
+++ spotNESS/user/static/user/spotness.js
@@ -1,11 +1,29 @@
-function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
-	var map = new google.maps.Map(document.getElementById('map'), {
-  	zoom: 4,
-  	center: uluru
-	});
-	var marker = new google.maps.Marker({
- 	 position: uluru,
-  	map: map
-	});
-}
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.643387, lng: 20.612224},
+          zoom: 11
+        });
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
